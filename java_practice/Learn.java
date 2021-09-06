e1cf63abf1e0d5a96fb987df0c3eb8df85068d0a
MapleWan <1041709112@qq.com>
2021-09-06 17:03:22 +0800

    node list problem update

--- java_practice/Learn.java
+++ java_practice/Learn.java
@@ -1,21 +1,54 @@
 package java_practice;
 
+import java.util.ArrayList;
+import java.util.Stack;
+
 public class Learn{
+
+    public static ArrayList<Integer> tmp = new ArrayList<>();
+    public static Stack<Integer> s_tmp = new Stack<>();
     public static void main(String[] args) {
-        String s = "We are happy";
-        // for(int i =0; i < s.length(); i++){
-        //     if()
-        // }
-        StringBuffer res = new StringBuffer();
-        for (char c : s.toCharArray()){
-            if(c == ' '){
-                res.append("%20");
-                continue;
-            }
-            res.append(c);
+        ListNode l1 = new ListNode(1);
+        ListNode l2 = new ListNode(3);
+        l1.next = l2;
+        ListNode l3 = new ListNode(2);
+        l2.next = l3;
+        l3.next = null;
+
+        ListNode head = l1;
+        
+        while(head !=null){
+            System.out.println(head.val);
+            s_tmp.push(head.val);
+            head = head.next;
         }
-        res.toString();
-        System.out.println(res);
-        // System.out.println(s.replaceAll(" ", "%20"));
+        
+        int[] res = new int[s_tmp.size()];
+
+        for (int i =0; i <= s_tmp.size(); i++){
+            res[i] = s_tmp.pop();
+            System.out.println(i);
+
+        }
+        
+        // for (int i : res) {
+        //     System.out.println(i);
+        // }
+        
+    }
+
+    public static void recur(ListNode node){
+        if(node == null) return;
+        recur(node.next);
+        tmp.add(node.val);
+        
+    }
+}
+
+class ListNode{
+    int val;
+    ListNode next;
+    ListNode(int x){
+        val = x;
     }
 }
--- learn_note/Algorithm_practice/链表/剑指 Offer 06. 从尾到头打印链表.md
+++ learn_note/Algorithm_practice/链表/剑指 Offer 06. 从尾到头打印链表.md
@@ -11,3 +11,75 @@
 
 # 解答
 
+方法1：递归
+
+```java
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    ArrayList<Integer> tmp = new ArrayList<>();
+    public int[] reversePrint(ListNode head) {
+        recur(head);
+        int[] res = new int[tmp.size()];
+        for(int i = 0; i < res.length; i++)
+            res[i] = tmp.get(i);
+        return res;
+    }
+
+    public void recur(ListNode node){
+        if(node == null) return;
+        recur(node.next);
+        tmp.add(node.val);
+        
+    }
+}
+```
+
+方法2(maple)：使用栈
+
+```java
+class Solution {
+    // ArrayList<Integer> tmp = new ArrayList<>();
+    Stack<Integer> s_tmp = new Stack<>();
+    public int[] reversePrint(ListNode head) {
+
+        while(head !=null){
+            s_tmp.push(head.val);
+            head = head.next;
+        }
+        
+        int size_s = s_tmp.size();        
+        int[] res = new int[size_s];
+        for (int i =0; i < size_s; i++){
+            res[i] = s_tmp.pop();
+        }
+        return res;
+    }
+
+}
+```
+
+栈思想的另一种使用方法：
+
+```java
+class Solution {
+    public int[] reversePrint(ListNode head) {
+        LinkedList<Integer> stack = new LinkedList<Integer>();
+        while(head != null) {
+            stack.addLast(head.val);
+            head = head.next;
+        }
+        int[] res = new int[stack.size()];
+        for(int i = 0; i < res.length; i++)
+            res[i] = stack.removeLast();
+    return res;
+    }
+}
+```
+
