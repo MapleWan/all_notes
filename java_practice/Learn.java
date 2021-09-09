fb6c0be4340851b6310d1fb5d9788524ea8bc9e5
MapleWan <1041709112@qq.com>
2021-09-09 18:30:04 +0800

    learn update

--- java_practice/Learn.java
+++ java_practice/Learn.java
@@ -1,54 +1,49 @@
 package java_practice;
 
-import java.util.ArrayList;
 import java.util.Stack;
 
 public class Learn{
 
-    public static ArrayList<Integer> tmp = new ArrayList<>();
-    public static Stack<Integer> s_tmp = new Stack<>();
+    // public static Stack<ListNode> stack_listNode = new Stack<>();
     public static void main(String[] args) {
         ListNode l1 = new ListNode(1);
-        ListNode l2 = new ListNode(3);
+        ListNode l2 = new ListNode(2);
         l1.next = l2;
-        ListNode l3 = new ListNode(2);
+        ListNode l3 = new ListNode(3);
         l2.next = l3;
-        l3.next = null;
+        ListNode l4 = new ListNode(4);
+        l3.next = l4;
+        ListNode l5 = new ListNode(5);
+        l4.next = l5;
+        l5.next = null;
 
         ListNode head = l1;
-        
-        while(head !=null){
-            System.out.println(head.val);
-            s_tmp.push(head.val);
-            head = head.next;
-        }
-        
-        int[] res = new int[s_tmp.size()];
 
-        for (int i =0; i <= s_tmp.size(); i++){
-            res[i] = s_tmp.pop();
-            System.out.println(i);
+        
 
-        }
-        
-        // for (int i : res) {
-        //     System.out.println(i);
-        // }
-        
+
     }
 
-    public static void recur(ListNode node){
-        if(node == null) return;
-        recur(node.next);
-        tmp.add(node.val);
-        
+    // 输入: 1->2->3->4->5->NULL
+    // 输出: 5->4->3->2->1->NULL
+    public static ListNode reverseList(ListNode head) {
+        Stack<ListNode> stack_listNode = new Stack<>();
+        ListNode res;
+        while(head != null){
+            stack_listNode.add(head);
+            head = head.next;
+        }
+        res = stack_listNode.pop();
+        while(stack_listNode != null){
+            stack_listNode
+        }
+
+        return res;
     }
 }
 
-class ListNode{
+class ListNode {
     int val;
     ListNode next;
-    ListNode(int x){
-        val = x;
-    }
+    ListNode(int x) { val = x; }
 }
